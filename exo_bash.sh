53636fc16acc5ca4677def8cb161155441820987
LiLHAOUDJ <lilhaoudj@gmail.com>
2025-08-28 10:49:37 +0100

    change_par_linux

--- exo_bash.sh
+++ exo_bash.sh
@@ -6,7 +6,7 @@ echo "Entrer un num pour le nb1: "; read nb1
 echo "Entrer un num pour le nb2 : "; read nb2
 echo "Entrer un num pour le nb3 : "; read nb3
 
-if $nb1 -eq $nb2; then
+if [$nb1 -eq $nb2]; then
 	echo "le nombre 1 est egal au nombre 2"
 elif $nb2 -lt $nb1; then
 	echo "Le nombre 2 est inferieur au nombre 1"
