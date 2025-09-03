9c3e743385e587e174e5efc557cb7633330baef1
LiLHAOUDJ <lilhaoudj@gmail.com>
2025-09-03 10:02:03 +0100

    Update exo_bash.sh

--- exo_bash.sh
+++ exo_bash.sh
@@ -6,13 +6,12 @@ echo "Entrer un num pour le nb1: "; read nb1
 echo "Entrer un num pour le nb2 : "; read nb2
 echo "Entrer un num pour le nb3 : "; read nb3
 
-if [$nb1 -eq $nb2]; then
-	echo "le nombre 1 est egal au nombre 2"
-elif $nb2 -lt $nb1; then
-	echo "Le nombre 2 est inferieur au nombre 1"
-elif $nb2 -gt $nb3; then
-	echo "le nombre 2 est supperieur au nombre 3"
-else
-	echo "La bonne condition n'est pas posé !"
-	#statements
+if [[ $nb1 -lt $nb2 ]]; then
+	echo "Nbr1 est inferieur à nb2 !"
+elif [[ $nb2 -lt $nb3 ]]; then
+	echo "nb2 est inferieur à nb3"
+elif [[ $nb1 -lt $nb3 ]]; then
+	echo "nb1 est inferieur à nb3"
+elif [[ $nb3 -lt $nb1 ]]; then
+	echo "nb3 est inferieur à nb1"
 fi
